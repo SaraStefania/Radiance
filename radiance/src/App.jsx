203258448f918f198ef9728ad93d3965cde4d120
sara040616 <sara.sierraramos@gmail.com>
2023-08-03 20:25:49 -0500

    Configuración final, se agrego Tailwind

---
+++ radiance/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="bg-blue-500 p-4 text-white">
+    ¡Hola, mundo! Este es un texto de ejemplo con estilos de Tailwind CSS.
+  </div>
+  )
+}
+
+export default App
